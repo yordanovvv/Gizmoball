a68ddb0cce860f8ba2086dd3fba00295ef3647c5
Lora Kiosseva <lora.kiosseva.2016@uni.strath.ac.uk>
2019-02-04 16:46:22 +0000

    Physics package, Ball

--- src/Model/Ball.java
+++ src/Model/Ball.java
@@ -1,22 +1,59 @@
 package Model;
-
+import java.awt.Color;
+import physics.Circle;
+import physics.Vect;
 
 import java.util.Observable;
-import java.util.Vector;
 
 public class Ball extends Observable {
 
-    int ID;
-    double radius;
-    //Vect velocity;
+    private Vect velocity;
+    private double radius;
+    private double xpos;
+    private double ypos;
+    private Color colour;
+    private int ID;
+    private boolean stopped;
 
-    private int getID(){
+    public Ball(double x, double y, double xv, double yv)
+    {
+        xpos = x;
+        ypos = y;
+        colour = Color.BLUE;
+        velocity = new Vect(xv, yv);
+        radius = 10;
+        stopped = false;
+    }
+
+    public Vect getVelo()
+    {
+        return velocity;
+    }
+
+    public void setVelo(Vect v)
+    {
+        velocity = v;
+    }
+
+    public double getRadius()
+    {
+        return radius;
+    }
+
+    public Circle getCircle()
+    {
+        return new Circle(xpos, ypos, radius);
+    }
+
+    public int getID(){
         return  ID;
     }
 
-    private boolean getState(){
-        return false;
+    public void setID(int id)
+    {
+        ID = id;
     }
+
 
 
 }
--- src/physics/Gravity.java
+++ src/physics/Gravity.java
@@ -1,4 +1,4 @@
-package Physics;
+package physics;
 
 public class Gravity {
 }
