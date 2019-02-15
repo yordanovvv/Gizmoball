e701b758260535e18e02979ceb854af18a4e9d9c
Nellie Oliver CSM2016 <nellie.oliver.2016@uni.strath.ac.uk>
2019-02-15 22:15:48 +0000

    added some flipper stuff - manual add as my old laptop is crap.

--- src/Controller/PlayListeners/FlipperKeyListener.java
+++ src/Controller/PlayListeners/FlipperKeyListener.java
@@ -1,8 +1,6 @@
 package Controller.PlayListeners;
 
 import Model.GizmoballModel;
-import Model.LeftFlipper;
-import Model.RightFlipper;
 import Model.iGizmo;
 
 import java.awt.event.KeyEvent;
@@ -14,33 +12,69 @@ public class FlipperKeyListener implements KeyListener {
     GizmoballModel model;
     char key;
     iGizmo flipper;
+    boolean isStopped;
 
     public FlipperKeyListener(String rot, GizmoballModel model, char key, iGizmo flipper){
         this.rot = rot;
         this.model = model;
         this.key = key;
         this.flipper = flipper;
+        this.isStopped = false;
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
         if (e.getKeyChar() == key ){
-            System.out.println("Flipper flip!");
-
-            flipper.rotate();
-
-            //flipper.rotate();
-            //flipper.flip();
+            moveFlipper("UP");
         }
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyPressed(KeyEvent e) {
 
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyChar() == key) {
+            moveFlipper("DOWN");
+        }
+    }
+
+    public void setIsStopped(boolean isStopped){
+        this.isStopped = isStopped;
+    }
+
+
+    public void moveFlipper(String direction){
+        if(!isStopped) {
+            if (direction.equals("UP") && flipper.getRotationAngle() == 0) {
+                move();
+                System.out.println(flipper.getRotationAngle());
+            } else if (direction.equals("DOWN") && flipper.getRotationAngle() == 90) {
+                move();
+            } else if (direction.equals("TICK")) {
+                tick();
+            } else if(direction.equals("RESET")) {
+                while(flipper.getRotationAngle()!= 90){tick();}
+                }
+            }
+        }
+
+    private void tick(){
+        flipper.rotate();
+        model.hasChanged();
+        model.notifyObservers();
+    }
+
+    private void move(){
+        for (int i = 0; i <=4; i++) {
+            tick();
+            try {
+                Thread.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
