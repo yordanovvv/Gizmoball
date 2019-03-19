e5aac6ea7aa190c5785af221c3adde7a44c26c0e
Lora Kiosseva <lora.kiosseva.2016@uni.strath.ac.uk>
2019-03-19 21:51:45 +0000

    setGravity and setFriction added to model

--- src/Model/Ball.java
+++ src/Model/Ball.java
@@ -18,6 +18,7 @@ public class Ball extends Observable {
     private double speed;
     private double friction;
 
+
     //fixed size. so size 1 for pixel
     public Ball(String id, double x, double y, double xv, double yv) {
         this.ID = id;
@@ -87,69 +88,29 @@ public class Ball extends Observable {
         return "Ball " + getID() + " " + Math.round(getExactX()/30*100)/100 + " " + Math.round(getExactY()/30*100)/100+ " " + Math.round(getVelo().x()/30)/100 + " " + Math.round(getVelo().y()/30)/100;
     }
 
-    public Vect applyFriction()
+    public void applyFriction(double mu1, double mu2)
     {
-        double mu = 0.025; //per second
-        double mu2 = 0.025; //per L
-        double delta_t = 0.05;
-        //Vect absVold = new Vect (abs(Vold.x()), abs(Vold.y()));
-
-        Vect Vnew  = this.getVelo().times(1 - (mu * delta_t) - (mu2 * abs(this.getVelo().length()) * delta_t/30));
-
-      //  Vect Vold = this.getVelo();
-      //  double x = Vold.x() * (1 - (mu * delta_t) - (mu2 * absVold.x() * delta_t));
-       // double y = Vold.y() * (1 - (mu * delta_t) - (mu2 * absVold.y() * delta_t));
-
-
-        friction = Vnew.length();
-
-        return Vnew;
-    }
-
-    public  void setFriction(double mu1, double mu2)
-    {
+        //double mu = 0.025; //per second
+        //double mu2 = 0.025; //per L
         double delta_t = 0.05;
         Vect Vnew  = this.getVelo().times(1 - (mu1 * delta_t) - (mu2 * abs(this.getVelo().length()) * delta_t/30));
+        friction = Vnew.length();
         this.setVelo(Vnew);
     }
+
 
     public double getFriction()
     {
         return friction;
     }
 
-  /*  public double applyFriction(double moveT)
-    {
-        double mu = 0.025; //per second
-        double mu2 = 0.025; //per L
-        double delta_t = 0.05; //what is a sufficiently small delta_t?
-
-        double Vold = this.calculateSpeed(delta_t);
-
-        double absVold = abs(Vold);
-        double Vnew = Vold * (1 - (mu * delta_t) - (mu2 * absVold * delta_t));
-
-        return Vnew;
-    }*/
-
-    public void applyGravity(double g, double time)
+    public void applyGravity(double gravity, double time)
     {
         Vect velocity = this.getVelo();
-        Vect grav = new Vect (0, g*constant*time);
-
+        Vect grav = new Vect (0, gravity*constant*time);
         this.setVelo(this.getVelo().plus(grav));
-
-       // double x = velocity.x();// + g; gravity only acts in the y direction
-        //double y = velocity.y()+g;
-
-        // velocity = new Vect(x,y);
-        //return velocity;
     }
 
-    public void setGravity(double grav){
-        Vect g = new Vect (0, grav);
-        this.setVelo(this.getVelo().plus(g));
-    }
 
     public double calculateSpeed(double time)
     {
@@ -159,13 +120,10 @@ public class Ball extends Observable {
         double y = velocity.y();
 
         //speed = sqrt(x*x + y*y)/timeInSecs;
-
         //speed = velocity.length()/30;
 
         speed = velocity.length()/timeInSecs;
-
         return speed;
-
     }
 
     public double getSpeed()
--- src/Model/GizmoballModel.java
+++ src/Model/GizmoballModel.java
@@ -23,10 +23,12 @@ public class GizmoballModel extends iModel {
     private iGizmo collisionGizmo = null;
     private boolean wallCollision = false;
     private double gravity = 25; //25L/sec^2
+    double mu1 = 0.025, mu2 = 0.025;
     boolean[][] spaces = new boolean[20][20];
     HashMap<iGizmo,Character> keyTriggers;
     HashMap<iGizmo,String> keyOrientation;
     private int wait = 0;
+
 
     public GizmoballModel() {
 
@@ -90,10 +92,11 @@ public class GizmoballModel extends iModel {
             if (ball != null && !ball.isStopped()) {
 
                 ball.applyGravity(gravity, moveTime);
-                ball.applyFriction();
+                ball.applyFriction(mu1, mu2);
 
                 CollisionDetails cd = timeUntilCollision(ball);
                 double tuc = cd.getTuc();
+                System.out.println("SPeed " + ball.getSpeed() );
 
                 if (tuc > moveTime) //no collision
                 {
@@ -1128,6 +1131,17 @@ public class GizmoballModel extends iModel {
         return gizmo.removeGizmoConnection(id);
     }
 
+    public void setGravity(double g){
+        gravity = g;
+        //Vect g = new Vect (0, grav);
+        // this.setVelo(this.getVelo().plus(g));
+    }
+
+    public  void setFriction(double mu1, double mu2)
+    {
+        this.mu1 = mu1;
+        this.mu2 = mu2;
+    }
 
 
 }
